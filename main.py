43970066be41687dd40c8e6570b6c0c3546fc1b4
LURAVI <61315321+luravi@users.noreply.github.com>
2024-08-20 16:06:47 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.py
@@ -0,0 +1,12 @@
+def on_forever():
+    music.play(music.string_playable("G B A G C5 B A B ", 120),
+        music.PlaybackMode.UNTIL_DONE)
+    maqueen.write_led(maqueen.LED.LED_LEFT, maqueen.LEDswitch.TURN_ON)
+    maqueen.write_led(maqueen.LED.LED_RIGHT, maqueen.LEDswitch.TURN_ON)
+    basic.pause(2000)
+    maqueen.write_led(maqueen.LED.LED_LEFT, maqueen.LEDswitch.TURN_OFF)
+    maqueen.motor_run(maqueen.Motors.ALL, maqueen.Dir.CW, 50)
+    maqueen.write_led(maqueen.LED.LED_RIGHT, maqueen.LEDswitch.TURN_OFF)
+    basic.pause(2000)
+    maqueen.motor_stop(maqueen.Motors.ALL)
+basic.forever(on_forever)
