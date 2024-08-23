9e10cfdbdc43182004ca8027c518f293a4c63790
LURAVI <61315321+luravi@users.noreply.github.com>
2024-08-23 11:49:28 +0200

    Actualizar pxt.json, main.blocks, main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
-basic.forever(function on_forever() {
+basic.forever(function () {
     music.play(music.stringPlayable("G B A G C5 B A B ", 120), music.PlaybackMode.UntilDone)
     maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
     maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
     basic.pause(2000)
     maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
-    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
     maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
-    basic.pause(2000)
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
+    basic.pause(1000)
     maqueen.motorStop(maqueen.Motors.All)
 })
